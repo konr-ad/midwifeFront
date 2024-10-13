43237d3ce4dcf66c61ecb43405e46693b63e685d
Konrad <konradk95@gmail.com>
2024-10-13 17:09:02 +0200

    added header and image-carousel components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {HeaderComponent} from './header/header.component';
+import {ImageCarouselComponent} from './image-carousel/image-carousel.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, ImageCarouselComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/image-carousel/image-carousel.component.ts
@@ -0,0 +1,30 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-image-carousel',
+  standalone: true,
+  imports: [],
+  templateUrl: './image-carousel.component.html',
+  styleUrl: './image-carousel.component.css'
+})
+export class ImageCarouselComponent implements OnInit {
+  images: string[] = [
+    "https://lactusia.pl/userdata/public/boxes/cee9459dd6cd8c8c1fa839bd59c5fbe8.jpg",
+    "https://lactusia.pl/userdata/public/boxes/f9ba16a044ec39a2f76c2be8f9362704.jpg",
+    "https://lactusia.pl/userdata/public/boxes/92fab392f1bc492a58384f47923fe404.jpg"
+  ];
+
+  currentImageIndex: number = 0;
+  currentImage: string = this.images[0]
+
+  ngOnInit() {
+    this.startImageRotation();
+  }
+
+  startImageRotation(): void {
+    setInterval(() => {
+        this.currentImageIndex = (this.currentImageIndex +1) % this.images.length;
+        this.currentImage = this.images[this.currentImageIndex];
+      }, 5000);
+  }
+}
