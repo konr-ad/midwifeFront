eecc57918f8d02b9daf97abcff04e9a97d70eabf
Konrad <konradk95@gmail.com>
2024-10-13 22:19:38 +0200

    added consult-list component

---
+++ src/app/consult-list/consult-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-consult-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './consult-list.component.html',
+  styleUrl: './consult-list.component.css'
+})
+export class ConsultListComponent {
+
+}
