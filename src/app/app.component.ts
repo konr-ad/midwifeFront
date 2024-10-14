0d91b834d958f0c641c8736faa03f46fc864e52e
Konrad <konradk95@gmail.com>
2024-10-14 23:30:19 +0200

    Added routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
 import {HeaderComponent} from './header/header.component';
 import {ImageCarouselComponent} from './image-carousel/image-carousel.component';
 import {TestimonialsComponent} from './testimonials/testimonials.component';
@@ -8,11 +8,12 @@ import {CourseListComponent} from './course-list/course-list.component';
 import {ContactComponent} from './contact/contact.component';
 import {FooterComponent} from './footer/footer.component';
 import {AboutComponent} from './about/about.component';
+import {CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, ImageCarouselComponent, TestimonialsComponent, ConsultListComponent, CourseListComponent, ContactComponent, FooterComponent, AboutComponent],
+  imports: [RouterOutlet, HeaderComponent, ImageCarouselComponent, TestimonialsComponent, ConsultListComponent, CourseListComponent, ContactComponent, FooterComponent, AboutComponent, CommonModule, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
