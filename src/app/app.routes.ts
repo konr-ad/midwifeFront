bfdbb3cce4bcac7591cbcc1e6249f29d24a59394
Konrad <konradk95@gmail.com>
2024-10-17 21:57:40 +0200

    implementation of 404component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import {SubConsultComponent} from './sub/sub-consult/sub-consult.component';
 import {HomeComponent} from './home/home.component';
 import {ConsultDetails1Component} from './sub/sub-consult/consult-details1/consult-details1.component';
+import {NotFoundComponent} from './not-found/not-found.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'consults', component: SubConsultComponent },
-  { path: 'consultdetails', component: ConsultDetails1Component }
+  { path: 'consultdetails', component: ConsultDetails1Component },
+  { path: '**', component: NotFoundComponent }
 ];
