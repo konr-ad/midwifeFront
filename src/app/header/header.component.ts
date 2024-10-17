93daf3bfb4b2f64fe86c8a845e45156f913a17a1
Konrad <konradk95@gmail.com>
2024-10-17 21:05:16 +0200

    added underline to navbar depending on the active route

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import {NgIf} from '@angular/common';
-import {RouterLink} from '@angular/router';
+import {RouterLink, RouterLinkActive} from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [
     NgIf,
-    RouterLink
+    RouterLink,
+    RouterLinkActive
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
