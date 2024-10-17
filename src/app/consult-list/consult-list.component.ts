7a12d1e4e6d7352db1de377743c7f78f6b1d7dbb
Konrad <konradk95@gmail.com>
2024-10-17 23:16:04 +0200

    fixed buttons destinations

--- src/app/consult-list/consult-list.component.ts
+++ src/app/consult-list/consult-list.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-consult-list',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './consult-list.component.html',
   styleUrl: './consult-list.component.css'
 })
--- src/app/course-list/course-list.component.ts
+++ src/app/course-list/course-list.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-course-list',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './course-list.component.html',
   styleUrl: './course-list.component.css'
 })
